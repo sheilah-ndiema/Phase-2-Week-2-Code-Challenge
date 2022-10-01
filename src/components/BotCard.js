2eaf3f29ebf48e66a8f265ea5f297644ccf96f54
Ndiema-S <you@example.com>
2022-10-01 23:36:15 +0300

    add click event

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -9,13 +9,14 @@ const botTypeClasses = {
   Captain: "icon star",
 };
 
-function BotCard({ bot }) {
+function BotCard({ bot, clickEvent, deleteBot }) {
   return (
     <div className="ui column">
       <div
         className="ui card"
         key={bot.id}
-        onClick={() => console.log("add code to connect event listener")}
+        onClick={() => clickEvent(bot)}
+
       >
         <div className="image">
           <img alt="oh no!" src={bot.avatar_url} />
